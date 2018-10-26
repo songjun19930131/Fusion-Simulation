3017f79e204e7f9261e592006c64263921ba6b79
Christopher van Hoecke <kitovh@gmail.com>
2018-10-25 21:50:06 -0400

    Constants.py

---
+++ Constants.py
@@ -0,0 +1,9 @@
+## File for all universal constants 
+
+# Universale Constants
+k      =   8.900000000 * 10**(  9)    # Colomb constant
+q      =   1.602176621 * 10**(-19)  # Proton Charge 
+m      =   1.672621898 * 10**(-27)  # Proton mass
+Radius =   8.768000000 * 10**(-16)  # Proton Radius sould be -16
+boltzman_constant = 1.38064852 * 10**(-23)
+strong_force_distance = 2.5 * 10**(-15)
