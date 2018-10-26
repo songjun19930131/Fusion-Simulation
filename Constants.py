3bd404bd7983dc0980575478a8f24baf548526a5
Christopher van Hoecke <kitovh@gmail.com>
2018-10-25 21:52:22 -0400

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -7,3 +7,13 @@ m      =   1.672621898 * 10**(-27)  # Proton mass
 Radius =   8.768000000 * 10**(-16)  # Proton Radius sould be -16
 boltzman_constant = 1.38064852 * 10**(-23)
 strong_force_distance = 2.5 * 10**(-15)
+
+# TANK VARIABLES
+Temperature = 100000000 # Temperature in Kelvin. 
+time        = 100
+delta_t     = 1*10**(-10)
+
+## Tank Dimensions
+Lx = 10 # x-length
+Ly = 10 # y-length
+Lz = 10 # z-lenght
